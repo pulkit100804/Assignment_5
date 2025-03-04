97b96869eb5e42ddefa7b55d2e9de9efbc4451c5
Pulkit Shrivastava <167713133+pulkit100804@users.noreply.github.com>
2025-03-04 22:08:17 +0530

    Uploaded rectangle.java

---
+++ Rectangle.java
@@ -0,0 +1,21 @@
+class Rectangle extends Shape {
+    double length, width;
+
+    Rectangle(double length, double width) {
+        super("Rectangle");
+        this.length = length;
+        this.width = width;
+    }
+
+    double calculateArea() {
+        return length * width;
+    }
+
+    double calculatePerimeter() {
+        return 2 * (length + width);
+    }
+
+    double calculateVolume() {
+        return 0; // Rectangle has no volume
+    }
+}
